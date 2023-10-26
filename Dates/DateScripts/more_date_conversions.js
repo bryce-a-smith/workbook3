2fa596fb80ac8365b0fbfe18a98c77a00e86eefd
BRYCE SMITH <a.bryce.smith.gmail.com>
2023-10-26 15:03:30 -0400

    date differences

--- Dates/DateScripts/more_date_conversions.js
+++ Dates/DateScripts/more_date_conversions.js
@@ -2,10 +2,10 @@
 
 const myBday = new Date("01/06/1994");
 const momBday = new Date("010 24 1967");
-const dadBday = new Date(1962, 9, 18);
+//months zero-based using values
+const dadBday = new Date(1962, 8, 18);
 
 console.log(myBday.toLocaleString());
 console.log(momBday.toLocaleString());
 
 console.log(dadBday.toLocaleString());
-//why does this one add 1 month?
